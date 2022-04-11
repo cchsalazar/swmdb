1443e460206b60575b664dca22ed7948d7a71dbc
Christian Salazar <cchsalazar@gmail.com>
2022-04-11 01:57:31 -0300

    add user test records.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,8 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        UserFactory::createOne(['email' => 'user1@example.com']);
+        UserFactory::createMany(10);
 
         $manager->flush();
     }
