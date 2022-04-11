28017a8ed04ad8479b7cb212bf2e92a82f2b29f7
Christian Salazar <cchsalazar@gmail.com>
2022-04-11 03:14:05 -0300

    implement authenticator methods.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,24 +2,57 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
+    private UserRepository $userRepository;
+    private RouterInterface $router;
+
+    public function __construct(UserRepository $userRepository, RouterInterface $router)
+    {
+        $this->userRepository = $userRepository;
+        $this->router = $router;
+    }
+
     public function authenticate(Request $request): Passport
     {
-        // TODO: Implement authenticate() method.
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        return new Passport(
+            new UserBadge($email, function ($userIdentifier) {
+                $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
+
+                if (!$user) {
+                    throw new UserNotFoundException();
+                }
+
+                return $user;
+            }),
+            new PasswordCredentials($password)
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        return new RedirectResponse(
+            $this->router->generate('app_film')
+        );
     }
 
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->router->generate('app_login');
+    }
 }
